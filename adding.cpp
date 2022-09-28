c35d19714b5679528dec25942d9d1cf22228de3c
lkotarski <l-kotarski@wp.pl>
2022-09-28 22:54:13 +0200

    Create adding.cpp

---
+++ adding.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int reverse(int n)
+{
+    int x = 0, y;
+    while (n != 0) {
+        y = n % 10;
+        x = x * 10 + y;
+        n /= 10;
+        
+    }
+    return x;
+}
+int main() {
+
+    unsigned  long int num1, num2, sum;
+    cin >> num1 >> num2;
+    cerr << reverse(num1) << " + " << reverse(num2) << endl;
+    sum = reverse(num1) + reverse(num2);
+    cout << reverse(sum) << endl;
+    return 0;
+
+    return 0;
+}
